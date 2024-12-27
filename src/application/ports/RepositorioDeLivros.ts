3ba0c7961a036141fe60504d78c566e44bb21c27
Matheus Silva <matheus.silva@beuni.com.br>
2024-12-27 16:28:49 -0300

    refactor RepositorioDeLivros interface

--- src/application/ports/RepositorioDeLivros.ts
+++ src/application/ports/RepositorioDeLivros.ts
@@ -3,4 +3,6 @@ import { Livro } from "../../domain/Livro";
 export interface RepositorioDeLivros {
   salvar(livro: Livro): Promise<void>;
   listar(): Promise<Livro[]>;
+  buscarPorISBN(isbn: string): Promise<Livro | null>;
+  buscarPorTitulo(titulo: string): Promise<Livro[] | null>;
 }
